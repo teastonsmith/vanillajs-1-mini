f4ba8ebb40a439f961642363cf26ba75961e48b3
teastonsmith2 <t.easton.smith2@gmail.com>
2019-04-22 12:12:43 -0600

    finished project

---
+++ index.js
@@ -0,0 +1,29 @@
+const element = document.getElementById('counter');
+
+let count = 0;
+
+function increase() {
+	count++;
+	element.innerText = count;
+}
+
+function decrease() {
+	count--;
+	element.innerText = count;
+}
+
+function reset() {
+	count = 0;
+	element.innerHTML = `<mark>${count}</mark>`;
+	// element.innerHTML = "<mark>" + count + "</mark>";
+}
+
+function selectTheme(theme) {
+	document.getElementsByTagName('body')[0].className = theme;
+	document.getElementsByTagName('main')[0].className = theme;
+	const buttons = document.getElementsByTagName('button');
+
+	for (let i = 0; i < buttons.length; i++) {
+		buttons[i].className = theme;
+	}
+}
